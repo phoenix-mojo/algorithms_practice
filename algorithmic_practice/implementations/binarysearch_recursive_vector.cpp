2f9009c78d707adb66c61d81043b8c02c1667fe0
phoenix-mojo <prasoon@Prasoons-MacBook-Pro.local>
2016-09-04 21:53:42 -0400

    recursiveBinarySearch

---
+++ algorithmic_practice/implementations/binarysearch_recursive_vector.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int binarySearch(vector<int>& myVector, int left, int right, int searchElement) {
+	if(left>right)
+		return -1;
+	else {
+		int mid = left + (right - left)/2;
+		if(myVector[mid] == searchElement)
+			return mid;
+		else if(myVector[mid]>searchElement)
+			return(binarySearch(myVector, left, mid, searchElement));
+		else
+			return(binarySearch(myVector, mid+1, right, searchElement));
+	}
+	
+}
+
+int main() {
+	vector<int> myVector;
+	int size, input, searchElement;
+	cin>>size;
+	cout<<"Input array: \n";
+	while(size-->0) {
+		cin>>input;
+		myVector.push_back(input);
+	}
+	cout<<"Input search element: \n";
+	cin>>searchElement;
+	int res = binarySearch(myVector, 0, myVector.size()-1, searchElement);
+	(res == -1) ? cout<<"Search element not found.\n" : cout<<"Search element found at position: "<<res<<endl; 
+	return 0;
+}
